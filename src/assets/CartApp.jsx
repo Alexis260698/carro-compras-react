a6ab341fcc7461ef1df9853eb76764dfdfa0226c
Alexis260698 <hena260698@gmail.com>
2023-12-30 21:02:52 -0600

    agregando rutas

--- src/assets/CartApp.jsx
+++ src/assets/CartApp.jsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "../Components/CartView";
 import { CatalogView } from "../Components/CatalogView";
 import { useItemCarts } from "../hooks/useItemsCart";
@@ -14,15 +15,24 @@ export const CartApp = () => {
 
             <div className="container">
                 <h3 className="my-4">CartAPP</h3>
-                <CatalogView handler={handlerAddProductCart} />
 
-                {cartItems?.length <= 0 || (
-                    <div className="my-4 w-50">
-                        <CartView items={cartItems}
-                            handlerDelete={handlerDeleteProduct} />
-                    </div>
-                )}
+                <Routes>
+                    <Route path="catalog" element={<CatalogView handler={handlerAddProductCart} />} />
+                    
+                    <Route path="cart" element={(
+                        cartItems?.length <= 0 ?
+                        <div className="alert alert-warning">No hay elementos en el carro</div> :
+                        (
+                            <div className="my-4 w-50">
+                                <CartView items={cartItems}
+                                    handlerDelete={handlerDeleteProduct} />
+                            </div>
+                        )
+                    )} />
 
+                    <Route path="/" element= { <Navigate to={'/catalog'}  /> } />
+
+                </Routes>
             </div>
         </>)
 }
